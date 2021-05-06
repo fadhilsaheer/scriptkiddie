ada0d65db0b053c6f0d7e86da7cca85b9054b241
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-06 17:53:08 +0530

    checked args

--- tools/portscanner/main.py
+++ tools/portscanner/main.py
@@ -12,10 +12,29 @@ from help import show_help
 def main():
     args = sys.argv
 
-    if len(args) == 3:
-        ip = args[1]
-        port = int(args[2])
-        scan(ip, port)
+    if len(args) >= 3:
+        ip = None
+        port = None
+        scan_multiple_ports = False
+        # scan(ip, port)
+        for idx in range(0, len(args) - 1):
+            option = args[idx]
+
+            if option == "-ip":
+                ip = args[idx + 1]
+            if option == "-p":
+                port = args[idx + 1]
+            if option == "-ps":
+                scan_multiple_ports = True
+                port = args[idx + 1]
+            else:
+                continue
+
+        if not ip or not port:
+            show_help()
+        else:
+            scan(ip, port, scan_multiple_ports)
+
     else:
         show_help()
 
