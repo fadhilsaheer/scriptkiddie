92d2fd457824fadafe9c54fbcc55ecb2dc8e378a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-06 17:59:50 +0530

    now arning

--- tools/portscanner/main.py
+++ tools/portscanner/main.py
@@ -16,28 +16,38 @@ def main():
         ip = None
         port = None
         scan_multiple_ports = False
-        # scan(ip, port)
+        no_warnings = False
+
         for idx in range(0, len(args) - 1):
             option = args[idx]
 
             if option == "-ip":
                 ip = args[idx + 1]
+
             if option == "-p":
                 port = args[idx + 1]
+
+            if option == "-nw":
+                no_warnings = True
+
             if option == "-ps":
                 scan_multiple_ports = True
                 port = args[idx + 1]
+
             else:
                 continue
 
         if not ip or not port:
             show_help()
         else:
-            scan(ip, port, scan_multiple_ports)
+            scan(ip, port, scan_multiple_ports, no_warnings)
 
     else:
         show_help()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except:
+        print_red("\nForced To Quit !!\n")
--- tools/portscanner/scanner.py
+++ tools/portscanner/scanner.py
@@ -1,20 +1,25 @@
 import socket
-from colors import print_green, print_red
+from colors import print_green, print_red, print_orange
 
 
-def scan_port(ip, port):
+def scan_port(ip, port, no_warnings):
     try:
         sock = socket.socket()
         sock.connect((ip, port))
         print_green(f"[+] {port} IS OPEN")
     except:
+        if no_warnings:
+            return
         print_red(f"[-] {port} IS CLOSE")
 
 
-def scan(ip, port, scan_multiple_ports):
+def scan(ip, port, scan_multiple_ports, no_warnings):
+    print_orange(f"[*] Scanning {str(ip)}")
+
     port = int(port)
+
     if scan_multiple_ports:
-        for prt in range(1, port):
-            scan_port(ip, prt)
+        for prt in range(1, port + 1):
+            scan_port(ip, prt, no_warnings)
     else:
-        scan_port(ip, port)
+        scan_port(ip, port, no_warnings)
