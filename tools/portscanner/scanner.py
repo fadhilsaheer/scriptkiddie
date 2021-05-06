f0aaf050f2f1eb0048fdfca86f4e3eb4264c5ee2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-06 17:52:58 +0530

    added feature for multple port scans

--- tools/portscanner/scanner.py
+++ tools/portscanner/scanner.py
@@ -2,10 +2,19 @@ import socket
 from colors import print_green, print_red
 
 
-def scan(ip, port):
+def scan_port(ip, port):
     try:
         sock = socket.socket()
         sock.connect((ip, port))
         print_green(f"[+] {port} IS OPEN")
     except:
         print_red(f"[-] {port} IS CLOSE")
+
+
+def scan(ip, port, scan_multiple_ports):
+    port = int(port)
+    if scan_multiple_ports:
+        for prt in range(1, port):
+            scan_port(ip, prt)
+    else:
+        scan_port(ip, port)
