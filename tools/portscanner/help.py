044f18e5c030dbe1ad92311e5b7d36c90b39adfa
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-06 17:42:47 +0530

    update help manual

--- tools/portscanner/help.py
+++ tools/portscanner/help.py
@@ -3,4 +3,10 @@ from colors import print_orange
 
 def show_help():
     print("\nHELP\n")
-    print_orange("usage: python3 portscanner.py <ip> <port>\n")
+    print_orange("usage: python3 portscanner.py <options>\n")
+    print("\nOPTIONS\n")
+    print("-ip <target-ip>")
+    print("-p <target-port>\n")
+
+    print_orange("-nw : No warnings")
+    print_orange("-ps <number> : scan a range of ports\n")
